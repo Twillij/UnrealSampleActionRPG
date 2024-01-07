7f75c5534d0bfb980c1fac1ff41d8fb6887a80e7
Will <silentwill29@gmail.com>
2024-01-07 15:53:14 +1100

    Initial commit of project updated to UE5.3

---
+++ Source/ActionRPG.Target.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ActionRPGTarget : TargetRules
+{
+	public ActionRPGTarget(TargetInfo Target)
+		: base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		ExtraModuleNames.AddRange(new string[] { "ActionRPG" });
+	}
+}
